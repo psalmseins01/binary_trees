028e665fef7cf7fa520e50f4b1d90ae575206a5c
Anthony Gbobo <psalmseins@gmail.com>
2024-03-01 20:10:30 -0800

    Function that goes through a binary tree using level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,67 +1,128 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_height - Measures the height of a binary tree.
- * @tree: Pointer to the root node of the tree to measure the height.
- *
- * Return: If tree is NULL, the function must return 0.
+ * binary_tree_height - Function that measures the height of a binary tree
+ * @tree: tree to go through
+ * Return: the height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		int lft, rht;
+	size_t l = 0;
+	size_t r = 0;
 
-		if (tree->right)
-			rht = 1 + binary_tree_height(tree->right);
-		if (tree->left)
-			lft = 1 + binary_tree_height(tree->left);
-		if (lft > rht)
-			return (lft);
-		else
-			return (rht);
+	if (tree == NULL)
+	{
+		return (0);
 	}
 	else
-		return (0);
-}
-/**
- * print_at_level - Prints nodes at a specific level.
- * @tree: Pointer to the root node of the tree to traverse.
- * @func: Pointer to a function to call for each node.
- * @level: Level at which to print nodes.
- */
-void print_at_level(const binary_tree_t *tree, void (*func)(int), int level)
-{
-	if (tree && func)
 	{
-		if (level == 1)
-			func(tree->n);
-		else
+		if (tree)
 		{
-			print_at_level(tree->left, func, level - 1);
-			print_at_level(tree->right, func, level - 1);
+			l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		}
+		return ((l > r) ? l : r);
 	}
-
 }
-
 /**
- * binary_tree_levelorder - Traverses a binary tree in level-order traversal.
- * @tree: Pointer to the root node of the tree to traverse.
- * @func: Pointer to a function to call for each node.
+ * binary_tree_depth - depth of specified node from root
+ * @tree: node to check the depth
+ * Return: 0 is it is the root or number of depth
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+}
+/**
+ * linked_node - this function makes a linked list from depth level and node
+ * @head: pointer to head of linked list
+ * @tree: node to store
+ * @level: depth of node to store
+ * Return: Nothing
+ */
+void linked_node(link_t **head, const binary_tree_t *tree, size_t level)
+{
+	link_t *new, *aux;
+
+	new = malloc(sizeof(link_t));
+	if (new == NULL)
+	{
+		return;
+	}
+	new->n = level;
+	new->node = tree;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		aux = *head;
+		while (aux->next != NULL)
+		{
+			aux = aux->next;
+		}
+		new->next = NULL;
+		aux->next = new;
+	}
+}
+/**
+ * recursion - goes through the complete tree and each stores each node
+ * in linked_node function
+ * @head: pointer to head of linked list
+ * @tree: node to check
+ * Return: Nothing by default it affects the pointer
+ */
+void recursion(link_t **head, const binary_tree_t *tree)
+{
+	size_t level = 0;
+
+	if (tree != NULL)
+	{
+		level = binary_tree_depth(tree);
+		linked_node(head, tree, level);
+		recursion(head, tree->left);
+		recursion(head, tree->right);
+	}
+}
+/**
+ * binary_tree_levelorder - output the nodes elements in a level-order
+ * @tree: Pointer to root node
+ * @func: Function pointer
+ * Return: return void
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t h = 0, i = 1;
+	link_t *head, *tmp;
+	size_t height = 0, count = 0;
 
-	if (tree && func)
+	if (!tree || !func)
 	{
-		h = binary_tree_height(tree);
-		while (i <= h + 1)
+		return;
+	}
+	else
+	{
+		height = binary_tree_height(tree);
+		head = NULL;
+		recursion(&head, tree);
+		while (count <= height)
 		{
-			print_at_level(tree, func, i);
-			i++;
+			tmp = head;
+			while (tmp != NULL)
+			{
+				if (count == tmp->n)
+				{
+					func(tmp->node->n);
+				}
+				tmp = tmp->next;
+			}
+			count++;
+		}
+		while (head != NULL)
+		{
+			tmp = head;
+			head = head->next;
+			free(tmp);
 		}
 	}
-
 }
